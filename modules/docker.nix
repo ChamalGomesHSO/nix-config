7fdc30cdb0d6452046b4716946d758eb2de39041
ChamalGomes <106123134+ChamalGomesHSO@users.noreply.github.com>
2025-04-21 21:07:08 +1000

    add docker daemon config

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,10 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  home.file.".docker/config.json".text = ''
+    home.file.".docker/config.json".text = ''
+        {
+        "experimental": "enabled",
+        "psFormat": "table {{.ID}}\\t{{.Image}}\\t{{.Status}}\\t{{.Names}}"
+        }
+    '';
+
+    home.file.".docker/daemon.json".text = ''
     {
-      "experimental": "enabled",
-      "psFormat": "table {{.ID}}\\t{{.Image}}\\t{{.Status}}\\t{{.Names}}"
+        "features": {
+        "buildkit": true
+        },
+        "experimental": true,
+        "storage-driver": "overlay2"
     }
-  '';
+    '';
 }
