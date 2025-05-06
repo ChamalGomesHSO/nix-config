ed71d339511dcc5ac08ae9fe0b949ae0599a2977
ChamalGomesHSO <chamalgomes166@gmail.com>
2025-05-06 01:12:19 +0000

    add uv alias

--- modules/bash.nix
+++ modules/bash.nix
@@ -30,6 +30,8 @@
       docker-rebuild = "docker-compose down -v && docker-compose up --build";
       gdp = "git checkout dev && git pull";
       go="cd /home/azureuser/repos && code .";
+      ven = "source .venv/bin/activate";
+      dven = "deactivate";
 
       # Alert alias for long running commands
       alert = "notify-send --urgency=low -i \"$([ $? = 0 ] && echo terminal || echo error)\" \"$(history|tail -n1|sed -e 's/^\\s*[0-9]\\+\\s*//;s/[;&|]\\s*alert$//')\"";
